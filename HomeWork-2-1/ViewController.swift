0b286505ac4b78bf0c70c1ed2974ddd351ef3c62
Olga Yurchuk <olga.yurchuk@xored.com>
2022-05-16 19:44:46 +0700

    Initial Commit

---
+++ HomeWork-2-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-2-1
+//
+//  Created by Olga Yurchuk on 16.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
